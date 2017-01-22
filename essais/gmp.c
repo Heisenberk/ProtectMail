8e09b887aeaf5698f5a96cec454120f179e7168e
Heiseberk <clement.caumes@orange.fr>
2017-01-22 10:34:49 +0100

    22012017

--- essais/gmp.c
+++ essais/gmp.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <gmp.h>
-#define TAILLE 25
+#define TAILLE 10
 
 libere_memoire(mpz_t p,mpz_t q,mpz_t n,mpz_t z,mpz_t e,mpz_t d,gmp_randstate_t state){
 	mpz_clear(p);
@@ -53,13 +53,6 @@ void determine_e(mpz_t z,gmp_randstate_t state,mpz_t e){
 	mpz_clear(pgcd);
 }
 
-/*int genere_d(int e,int z){
-	int d=0;
-	while((e*d)%z!=1){
-		d=d+1;
-	}
-	return d;
-}*/
 
 //CALCUL DE LINVERSE DE E MOD Z
 void determine_d(mpz_t p,mpz_t q,mpz_t n,mpz_t z,mpz_t e,mpz_t d,gmp_randstate_t state){
@@ -74,6 +67,68 @@ void determine_d(mpz_t p,mpz_t q,mpz_t n,mpz_t z,mpz_t e,mpz_t d,gmp_randstate_t
 void affiche_cles(mpz_t e,mpz_t d,mpz_t n){
 	gmp_printf("CLE PUBLIQUE (%Zd,%Zd)\n",e,n);
 	gmp_printf("CLE PRIVEE   (%Zd,%Zd)\n",d,n);
+	//printf("A\n");
+}
+
+void encrypt(char* nomFichier,mpz_t n,mpz_t e){
+	FILE* f1=fopen(nomFichier,"r");
+	if(f1==NULL){
+		printf("FICHIER INEXISTANT\n");
+		exit(1);
+	}
+	FILE* f2=fopen("message_chiffre","w");
+	if(f1==NULL){
+		printf("FICHIER IMPOSSIBLE A CREER\n");
+		exit(1);
+	}
+	char c;double ascii;mpz_t m,u;int test;
+	mpz_init(m);mpz_init(u);
+	do{
+		c=fgetc(f1);
+		ascii=c;
+		if(c!=EOF){
+			mpz_set_d(m,ascii);
+			mpz_powm(u,m,e,n);
+			test=gmp_fprintf(f2,"%Zd ",u);
+		}
+	}while(c!=EOF);
+	fclose(f1);
+	fclose(f2);
+	mpz_clear(m);
+	mpz_clear(u);
+	printf("\nMESSAGE CHIFFRE\n");
+}
+
+void decrypt(char* nomFichier,mpz_t n,mpz_t d){
+	FILE* f1=fopen(nomFichier,"r");
+	if(f1==NULL){
+		printf("FICHIER INEXISTANT\n");
+		exit(1);
+	}
+	FILE* f2=fopen("message_dechiffre","w");
+	if(f1==NULL){
+		printf("FICHIER IMPOSSIBLE A CREER\n");
+		exit(1);
+	}
+	char c;double ascii;mpz_t m,u;int test;unsigned long int conv;int car;
+	mpz_init(m);mpz_init(u);
+	do{
+		c=fgetc(f1);
+		if((c!=EOF)&&(isdigit(c))){
+			ungetc(c,f1);
+			test=gmp_fscanf(f1,"%Zd",m);
+			mpz_powm(u,m,d,n);
+			conv=mpz_get_ui(u);
+			car=conv;
+			fprintf(f2,"%c",car);
+			while(c!=' ') c=fgetc(f1);
+		}
+	}while(c!=EOF);
+	fclose(f1);
+	fclose(f2);
+	mpz_clear(m);
+	mpz_clear(u);
+	printf("\nMESSAGE DECHIFFRE\n");
 }
 
 int main(int argc,char**argv){
@@ -97,6 +152,7 @@ int main(int argc,char**argv){
 	determine_e(z,state,e);
 	determine_d(p,q,n,z,e,d,state);
 	
+	printf("\n");
 	gmp_printf("p=%Zd\n",p);
 	gmp_printf("q=%Zd\n",q);
 	gmp_printf("n=%Zd\n",n);
@@ -105,6 +161,8 @@ int main(int argc,char**argv){
 	gmp_printf("d=%Zd\n",d);
 	
 	affiche_cles(e,d,n);
+	encrypt("test",n,e);
+	decrypt("message_chiffre",n,d);
 	
 	libere_memoire(p,q,n,z,e,d,state);
 }
